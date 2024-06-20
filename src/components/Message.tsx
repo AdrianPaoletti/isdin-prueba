0ea3248eaf9dca534a48a5abeef0f3bbd2bcaa7b
Adrian <apaoletti@seidor.es>
2024-06-20 13:42:05 +0200

    prueba

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -7,7 +7,7 @@ const Message = ({ notification }: any) => {
           {/* image is optional */}
           {notification.image && (
             <div id="imageContainer">
-              <img src={notification.image} width={100} />
+              <img src={notification.image} width={100} alt="prueba"/>
             </div>
           )}
           <span>{notification.title}</span>
